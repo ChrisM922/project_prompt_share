d7f9eeab9ff577c3bbaabc5ba3476bef33fee915
Christopher Mace <mace.christop@mm-bbs.de>
2023-07-09 11:28:00 +0200

    small global css changes

--- app/page.jsx
+++ app/page.jsx
@@ -3,10 +3,12 @@ import Feed from "@components/Feed";
 const Home = () => {
   return (
     <section className='w-full flex-center flex-col'>
-      <h1 className='head_text text-center'>
+      <h1 className='head_text text-center max-sm:whitespace-normal mx-5'>
         Discover & Share
         <br className='max-md:hidden' />
-        <span className='orange_gradient text-center'>AI-Powered Promps</span>
+        <span className='orange_gradient text-center max-sm:whitespace-normal mx-5'>
+          AI-Powered Promps
+        </span>
       </h1>
       <p className='mt-10'>
         PromptShare is an open-source AI prompting tool for the world to
